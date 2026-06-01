62a8e1c35800fbfb921de9a18e6318dd6744166e
Arthur1a <165918969+MARCOSANDRE1C@users.noreply.github.com>
2026-06-01 12:29:35 -0300

    sss

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-function irPagina(pagina){
-    window.location.href = pagina;
+function mostrar(id){
+
+    const paginas =
+        document.querySelectorAll(".pagina");
+
+    paginas.forEach(pagina=>{
+        pagina.classList.remove("ativa");
+    });
+
+    document
+        .getElementById(id)
+        .classList.add("ativa");
 }
