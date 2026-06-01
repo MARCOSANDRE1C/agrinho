c5cf801d0272cb6cf4356e66436a87f9f4099cc2
Arthur1a <165918969+MARCOSANDRE1C@users.noreply.github.com>
2026-06-01 12:35:43 -0300

    sss

--- main.js
+++ main.js
@@ -1,24 +1,13 @@
 function mostrar(id){
 
-    document.querySelectorAll(".pagina")
-        .forEach(p => p.classList.remove("ativa"));
+    const paginas =
+        document.querySelectorAll(".pagina");
 
-    document.getElementById(id)
+    paginas.forEach(pagina=>{
+        pagina.classList.remove("ativa");
+    });
+
+    document
+        .getElementById(id)
         .classList.add("ativa");
 }
-
-/* FAQ abrir e fechar */
-document.addEventListener("click", (e) => {
-
-    if(e.target.classList.contains("faq-btn")){
-
-        const resposta = e.target.nextElementSibling;
-
-        if(resposta.style.display === "block"){
-            resposta.style.display = "none";
-        } else {
-            resposta.style.display = "block";
-        }
-    }
-
-});
